4ad1099aead2843304ac09ff04e7f0d24cd722b3
Amy Trick <amyk.trick@gmail.com>
2020-12-08 05:26:00 +0000

    simplified modal text box

--- server.py
+++ server.py
@@ -145,8 +145,8 @@ def print_date(photo_id):
 def upload_new_photo():
     """User selects new photo to upload"""
     image = request.files['photo-upload']
-    #result = cloudinary.uploader.upload(image, image_metadata=True, categorization = "google_tagging", auto_tagging = 0.6)
-    #result = cloudinary.uploader.upload(image, image_metadata=True, categorization = "aws_rek_tagging", auto_tagging = 0.6)
+    # result = cloudinary.uploader.upload(image, image_metadata=True, categorization = "google_tagging", auto_tagging = 0.6)
+    # result = cloudinary.uploader.upload(image, image_metadata=True, categorization = "aws_rek_tagging", auto_tagging = 0.7)
     result = cloudinary.uploader.upload(image, image_metadata=True)
     #result["tags"] = ['forest','adventure']
     user_id = session['user_id']
