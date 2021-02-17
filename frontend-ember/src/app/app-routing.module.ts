c00f04e8a4d0ca2383d82d8f030b08992c7d5972
Amy Trick <amyk.trick@gmail.com>
2021-02-16 21:34:39 -0800

    routing to album detail page

--- frontend-ember/src/app/app-routing.module.ts
+++ frontend-ember/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './albums/albums.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: AlbumsComponent },
+  { path: 'album/:id', component: AlbumDetailsComponent },
+  { path: 'photo/:id', component: PhotoDetailsComponent }
+  // album/album_id
+  // photo/photo_id
+  // search/rating
+  // search/tag
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-ember/src/app/app.module.ts
+++ frontend-ember/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { AlbumDetailsComponent } from './album-details/album-details.component';
 import { PhotoDetailsComponent } from './photo-details/photo-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlbumsComponent,
     AlbumDetailsComponent,
-    PhotoDetailsComponent
+    PhotoDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     HttpClientModule,
