dcb2b703c960c31f141b4f6d33933d90fa187f82
Amy Trick <amyk.trick@gmail.com>
2021-02-28 20:42:40 -0800

    Resolve CORS errors - configuring the proxy for frontend backend communication - Preventing api calls with uninitialized data

--- frontend-ember/src/app/shared-photos.service.ts
+++ frontend-ember/src/app/shared-photos.service.ts
@@ -15,14 +15,21 @@ export class SharedPhotosService {
   private photo = new BehaviorSubject<Photo>(<Photo>{});
   currentPhoto = this.photo.asObservable();
 
-  currentIdx: any = 0
-
+  private currentIdx: any = 0
+  private has_current_photo: boolean = false;
   constructor(private http: HttpClient) {
   }
 
+  private resetCurrentPhoto() {
+    this.photo.next(<Photo>{});
+    this.has_current_photo = false;
+  }
+
   updateCurrentRating(newRating: number): void {
-    this.photo.value.rating = newRating;
-    this.http.post(`${API_URL}/update-rating`, { photo_id: this.photo.value.photo_id, rating: newRating }).subscribe(data => { });
+    if (this.has_current_photo) {
+      this.photo.value.rating = newRating;
+      this.http.post(`${API_URL}/update-rating`, { photo_id: this.photo.value.photo_id, rating: newRating }).subscribe();
+    }
   }
 
   nextPhoto() {
@@ -38,15 +45,17 @@ export class SharedPhotosService {
   setPhoto(photo_id: number) {
     this.currentIdx = this.photos.value.findIndex(photo => photo.photo_id == photo_id);
     this.photo.next(this.photos.value[this.currentIdx]);
+    this.has_current_photo = true;
   }
 
   updatePhotos(photos: Photo[]) {
     this.photos.next(photos);
+    this.resetCurrentPhoto();
   }
 
   addToAlbum(album_id: number) {
     console.log(`shared ${album_id}, not yet made the API call `);
-    this.http.post(`${API_URL}/add-to-album`, { photo_id: this.photo.value.photo_id, album_id: album_id }).subscribe(data => { });
+    this.http.post(`${API_URL}/add-to-album`, { photo_id: this.photo.value.photo_id, album_id: album_id }).subscribe();
 
   }
 }
