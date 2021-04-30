4f0d80ea6189cb1e5fe569583cc1393a29304cde
Amy Trick <amyk.trick@gmail.com>
2021-04-29 20:36:09 -0700

    imported bootstrap modal component

--- frontend-ember/src/app/login-page/login-page.component.ts
+++ frontend-ember/src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  content: any;
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
   }
 
 }
